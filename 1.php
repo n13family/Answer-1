f5a9f95f9ae5af1b533b44089d409b0d031087fa
n13family <42956023+n13family@users.noreply.github.com>
2019-05-04 13:28:59 +0700

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,33 @@
+<?php
+function biodata($name, $address, $hobbies, $is_married, $school, $skills){
+
+    $biodata = array(
+    	"Name" => $name,
+		"Address" => $address,
+		"hobbies" => $hobbies,
+		"is_married" => $is_married,
+		"school" => (object)$school,
+		"skill" => new ArrayObject($skills)
+		);
+
+    return json_encode($biodata);
+    
+}
+
+		$name = "Bambang Priyanto";
+		$address = "JL ITIK XIII BLOK I PTI NO 41, BEKASI , JAWA BARAT, 17158";
+		$hobbies = array(
+			"Coding",
+			"Playing a game"
+		);
+		$is_married = false;
+		$school = array("highSchool" => "SMA MANDALAHAYU BEKASI", "university" => "null");
+		$skills = array(
+			"0" => "PHP",
+			"1" => "JavaScript",
+			"2" => "MySql"
+		);
+
+		print(biodata($name, $address, $hobbies, $is_married, $school, $skills));
+
+?>
