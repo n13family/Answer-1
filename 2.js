671c141eaeaf5ede555ba1e1a92478a287896d24
n13family <42956023+n13family@users.noreply.github.com>
2019-05-04 19:37:48 +0700

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,5 +1,5 @@
   function is_username_valid(username){
-		if((/^[a-z]+$/.test(username)) == true && (username.length == 8) == true)
+		if((/^[a-z]{8}$/.test(username)) == true)
 		{
 			return true;
   
@@ -10,8 +10,8 @@
         }
   }
 
-  function is_password_valid(username){
-		if((/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(username)) == true)
+  function is_password_valid(password){
+		if((/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8}$/.test(password)) == true)
 		{
 			return true;
   
